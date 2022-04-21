a2b6cad345cb931586afcb0a8d4c4f918c88a074
Seokhun-Yang <ddayang1234@gmail.com>
2022-04-21 22:49:55 +0700

    commands.yml 생성, 저장, 불러오기 등의 메소드를 담은 클래스 추가

---
+++ src/main/java/net/teamuni/playerjoinandleave/UtilToolCommands.java
@@ -0,0 +1,41 @@
+package net.teamuni.playerjoinandleave;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+public class UtilToolCommands {
+
+    private static final UtilTool main = UtilTool.getPlugin(UtilTool.class);
+    private static File file;
+    private static FileConfiguration commandsFile;
+
+    public static void createCommandsYml() {
+        file = new File(Bukkit.getServer().getPluginManager().getPlugin("UtilTool").getDataFolder(), "commands.yml");
+
+        if (!file.exists()) {
+            file.getParentFile().mkdirs();
+            main.saveResource("commands.yml", false);
+        }
+        commandsFile = YamlConfiguration.loadConfiguration(file);
+    }
+
+    public static FileConfiguration get() {
+        return commandsFile;
+    }
+
+    public static void save() {
+        try {
+            commandsFile.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void reload() {
+        commandsFile = YamlConfiguration.loadConfiguration(file);
+    }
+}
