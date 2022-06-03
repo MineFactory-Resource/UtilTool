c3b3051b6b74a4fadf8de98216934f9ca7476d8f
Seokhun-Yang <ddayang1234@gmail.com>
2022-06-03 22:49:03 +0700

    플레이어의 UUID를 저장하는 yml파일을 manage하는 클래스 추가

---
+++ src/main/java/net/teamuni/playerjoinandleave/PlayerUuidManager.java
@@ -0,0 +1,41 @@
+package net.teamuni.playerjoinandleave;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PlayerUuidManager {
+
+    private static final UtilTool main = UtilTool.getPlugin(UtilTool.class);
+    private static File file;
+    private static FileConfiguration playersFile;
+
+    public static void createCommandsYml() {
+        file = new File(Bukkit.getServer().getPluginManager().getPlugin("UtilTool").getDataFolder(), "players.yml");
+
+        if (!file.exists()) {
+            file.getParentFile().mkdirs();
+            main.saveResource("players.yml", false);
+        }
+        playersFile = YamlConfiguration.loadConfiguration(file);
+    }
+
+    public static FileConfiguration get() {
+        return playersFile;
+    }
+
+    public static void save() {
+        try {
+            playersFile.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void reload() {
+        playersFile = YamlConfiguration.loadConfiguration(file);
+    }
+}
