9c94f23cebdcbc52e850d0708b31a671b38eae26
Fade <jjw4016@gmail.com>
2022-08-15 17:12:06 +0900

    쿨타임 클래스 변수명 줄이기

--- src/main/java/net/teamuni/playerjoinandleave/BroadCasterCooldown.java
+++ src/main/java/net/teamuni/playerjoinandleave/BroadCasterCooldown.java
@@ -14,18 +14,18 @@ public class BroadCasterCooldown {
     }
 
     //쿨타임 설정
-    public static void setCooldown(Player player, int seconds){
+    public static void setCooldown(Player p, int seconds){
         double delay = System.currentTimeMillis() + (seconds * 1000L);
-        cooldowns.put(player.getUniqueId(), delay);
+        cooldowns.put(p.getUniqueId(), delay);
     }
     //쿨타임 계산
-    public static int getCooldown(Player player){
-        return Math.toIntExact(Math.round((cooldowns.get(player.getUniqueId()) - System.currentTimeMillis())/1000));
+    public static int getCooldown(Player p){
+        return Math.toIntExact(Math.round((cooldowns.get(p.getUniqueId()) - System.currentTimeMillis())/1000));
     }
 
     //쿨타임 체크
-    public static boolean checkCooldown(Player player){
-        if(!cooldowns.containsKey(player.getUniqueId()) || cooldowns.get(player.getUniqueId()) <= System.currentTimeMillis()){
+    public static boolean checkCooldown(Player p){
+        if(!cooldowns.containsKey(p.getUniqueId()) || cooldowns.get(p.getUniqueId()) <= System.currentTimeMillis()){
             return true;
         }
         return false;
