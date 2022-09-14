138b85650b385ae19e2e2be64ee306c8594bf67e
Fade <jjw4016@gmail.com>
2022-09-14 19:39:35 +0900

    MessageManager 유지보수 좋게 수정

--- src/main/java/net/teamuni/playerjoinandleave/MessagesManager.java
+++ src/main/java/net/teamuni/playerjoinandleave/MessagesManager.java
@@ -1,15 +1,24 @@
 package net.teamuni.playerjoinandleave;
 
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Map;
+import org.bukkit.configuration.ConfigurationSection;
 
 public class MessagesManager {
     private static final UtilTool main = UtilTool.getPlugin(UtilTool.class);
     private static File file;
     private static FileConfiguration messagesFile;
+
+
 
     public static void createMessagesYml() {
         file = new File(main.getDataFolder(), "messages.yml");
@@ -20,10 +29,11 @@ public class MessagesManager {
         messagesFile = YamlConfiguration.loadConfiguration(file);
     }
 
-    public static FileConfiguration get() {
-        return messagesFile;
-    }
 
+    public static String get(String path) {
+        String value = messagesFile.getString(path);
+        return ChatColor.translateAlternateColorCodes('&',value);
+    }
     public static void save() {
         try {
             messagesFile.save(file);
